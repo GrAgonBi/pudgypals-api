1f217c1ecfc012af5b789bfae1b1cd0a36d8ad05
GrAgonBi <abiweiyang0913@gmail.com>
2024-03-20 20:45:18 -0400

    instantiate express and have app listen for HTTP requests from certain port

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-const bycrypt = require("bcryptjs");
+// const bycrypt = require("bcryptjs");
 
-console.log("user1:", bycrypt.hashSync("user1"));
-console.log("user2:", bycrypt.hashSync("user2"));
+// // console.log("user1:", bycrypt.hashSync("user1"));
+// // console.log("user2:", bycrypt.hashSync("user2"));
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+
+//access the req body
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Welcome to App");
+});
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`); // may need change url when deployed
+});
