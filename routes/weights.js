7c3d2b25701c1f5e525093c21f42e28135de8dfa
GrAgonBi <abiweiyang0913@gmail.com>
2024-03-24 01:20:40 -0400

    create router for weightRoute

---
+++ routes/weights.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const { authorize } = require("../controllers/authorize");
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
+const weightRouter = require("./routes/weights");
 
 //access the req body
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
+app.use("/weight", weightRouter);
 
 //test for initial setup
 app.get("/", (req, res) => {
