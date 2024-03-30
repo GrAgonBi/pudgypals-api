3bd098d587816ec69e7196dd5ca6ccde337061b3
GrAgonBi <abiweiyang0913@gmail.com>
2024-03-30 11:38:56 -0400

    revise knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,8 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    charset: "utf8",
+    database: process.env.DB_DATABASE,
   },
 };
